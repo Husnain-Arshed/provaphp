4470270ff6a19185846ddd020ac23658f1d9806a
Husnain Arshed <81387828+Husnain-Arshed@users.noreply.github.com>
2022-10-10 11:21:34 +0200

    Create pratica.php

---
+++ pratica.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title> Richiesta </title>
+</head>
+<body>
+<!–Form html di input–>
+<h2> Conversione in miglia </h2>
+<form action=”pratica.php” method=”get”> Lunghezza in km: <input type=”text” name=”km”><br>
+<input type=”submit” value=”Converti”>
+</form>
+
+<?php
+if($_GET) {
+
+
+$miglia =$_GET[“km”]/1.6093;
+
+echo “<h3> Chilometri = “.$_GET[“km”].”</h3>”;  echo “corrispondono a  Miglia : “;
+echo ($miglia);
+}
+?>
+</body> </html>
